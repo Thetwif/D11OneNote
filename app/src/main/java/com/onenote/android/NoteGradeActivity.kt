de9b2a4817ed0ffd81c653bf643cc99272f5b85f
Thetwif <thetwif.moser@axtesys,at>
2025-01-02 19:02:20 +0100

    added comments, added grading feature, added images and locations, updated colors and made it funnier

---
+++ app/src/main/java/com/onenote/android/NoteGradeActivity.kt
@@ -0,0 +1,72 @@
+package com.onenote.android
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ImageView
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+class NoteListActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
+
+    private lateinit var listView: ListView
+    private lateinit var adapter: NoteAdapter
+    private lateinit var db: Database
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_list)
+
+        // Set up toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        // Find views by ID
+        listView = findViewById(R.id.listView)
+
+        // Init database
+        db = Database(this)
+
+        // Set adapter
+        adapter = NoteAdapter(this, db.getAllNotes())
+        listView.adapter = adapter
+        listView.onItemClickListener = this
+        //currentPath = ""
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Reload notes
+        adapter.notes = db.getAllNotes()
+        adapter.notifyDataSetChanged()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_list, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add) {
+
+            // Open NoteEditActivity
+            val intent = Intent(this, NoteEditActivity::class.java)
+            startActivity(intent)
+        } else if (item.itemId == R.id.grade) {
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, id: Long) {
+        val intent = Intent(this, NoteEditActivity::class.java)
+        intent.putExtra("id", id)
+        startActivity(intent)
+    }
+}
